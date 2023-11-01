725df4642d838645e5e327f0f69b71304c67a11b
nuunnunn <gnr9706@gmail.com>
2023-11-01 18:34:47 +0900

    2023.11.1_2

--- js/jsap.js
+++ js/jsap.js
@@ -1,60 +1,52 @@
 //스크롤 방향
 gsap.registerPlugin(ScrollTrigger);
-gsap.defaults({ease: "power1", duration: 3});
+// gsap.defaults({ease: "power1", duration: 3});
 
-const tl = gsap.timeline();
-tl.from(".section02", {yPercent: 100})
-  .from(".section03", {xPercent: -100})
-  .from(".section04", {xPercent: 100})
-  .from(".section05", {yPercent: 100});
+// const tl = gsap.timeline();
+// tl.from(".section03", {xPercent: 100})
+//   .from(".section04", {xPercent: 100})
+//   .from(".section05", {yPercent: -100});
 
-ScrollTrigger.create({
-	animation: tl,
-  trigger: "#container",
-  start: "top top", 
-  end: "+=4000",
-  pin: true,
-	scrub: 2
-});
-
-
-
-
-// const pages = gsap.utils.toArray(".page");
-// const pagesAmount = pages.length;
-// const proxy = document.createElement("div");
-
-// const t = gsap.to(pages, {
-//   xPercent: -100 * (pagesAmount - 1),
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: ".horizontal-section",
-//     start: "top top",
-//     end: "+=" + 100 * pagesAmount + "%",
-//     scrub: true,
-//     pin: true,
-//     onUpdate: (self) => {
-//       // console.log(self.scroll(), self.start, self.start - self.scroll());
-//       gsap.set(proxy, { x: self.start - self.scroll() });
-//       d[0].update();
-//     },
-//     markers: {
-//       startColor: "white",
-//       endColor: "white"
-//     }
-//   }
+// ScrollTrigger.create({
+// 	animation: tl,
+//   trigger: "#container",
+//   start: "top top", 
+//   end: "+=4000",
+//   markers: true,
+//   pin: true,
+// 	scrub: 2
 // });
 
 
 
-$(window).scroll(function () {
-  var currentScrollTop = $(this).scrollTop();
-  // console.log(currentScrollTop)
-  if(150 < currentScrollTop){
-    $('.section01').children('.content').fadeOut()
-  }else if(0< currentScrollTop < 149){
-    $('.section01').children('.content').fadeIn()
+const pages = gsap.utils.toArray(".page");
+const pagesAmount = pages.length;
+const proxy = document.createElement("div");
+
+pages.map
+
+
+gsap.timeline({
+  scrollTrigger: {
+    trigger: ".container",
+    start: "top top",
+    end: "+=" + 100 * pagesAmount + "%",
+    scrub: 2,
+    pin: true,
+    onUpdate: (self) => {
+      // console.log(self.scroll(), self.start, self.start - self.scroll());
+      gsap.set(proxy, { x: self.start - self.scroll() });
+      // d[0].update();
+    },
+    markers: {
+      startColor: "white",
+      endColor: "white"
+    }
   }
+}).to(pages, {
+  xPercent: -100 * (pagesAmount - 1),
+  ease: "none",
+  
 });
 
 
@@ -65,12 +57,12 @@ $(function(){
   var win_w = $(window).width();
   
   //각 섹션까지의 너비와 높이 값 속성으로 넣어줌
-  $('.page').each(function(index){
-    $(this).attr("data-hindex",win_h * index);
-  });
-  $('.page').each(function(index){
-    $(this).attr("data-windex",win_w * index);
-  });
+  // $('.page').each(function(index){
+  //   $(this).attr("data-hindex",win_h * index);
+  // });
+  // $('.page').each(function(index){
+  //   $(this).attr("data-windex",win_w * index);
+  // });
 
   // $('.page').on("mousewheel",function(e){
   //   //속성값 불러옴
@@ -90,4 +82,16 @@ $(function(){
   //     $('.section01').children('.content').fadeOut()
   //   }
   // });
+});
+
+
+
+$(window).scroll(function () {
+  var currentScrollTop = $(this).scrollTop();
+  // console.log(currentScrollTop)
+  if(0< currentScrollTop && currentScrollTop < 99){
+    $('.section01').children('.content').stop().fadeIn()
+  }else if(currentScrollTop>100){
+    $('.section01').children('.content').stop().fadeOut()
+  }
 });

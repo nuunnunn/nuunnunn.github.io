5c9986319e853016b387bad437947de43487bf5b
nuunnunn <gnr9706@gmail.com>
2023-11-10 18:56:17 +0900

    2023.11.10_2

--- js/scroll.js
+++ js/scroll.js
@@ -6,11 +6,13 @@ $(window).scroll(function () {
     //sun img 스크롤하면 따라옴
     if (3200 < currentScrollTop && currentScrollTop < 5800) {
         $('.sun').css({ left: `${currentScrollTop - 3200}px`, opacity: 1 });
-    } else if (currentScrollTop < 3200) {
-        $('.sun').css({ left: `-300px`, opacity: 1 });
-    } else if (5800 < currentScrollTop) {
+    } 
+    else if (5800 < currentScrollTop) {
         $('.sun').css({ opacity: 0 })
     }
+    // else if (currentScrollTop < 3200) {
+    //     $('.sun').css({ left: `0px`, opacity: 1 });
+    // } 
 
 
     //티켓 스크롤
@@ -21,13 +23,13 @@ $(window).scroll(function () {
             $('.pj_ticket').css({transform: `translateX(300px)`});
         },function(){
             $(this).css({transform:'scale(1)'});
-            $('.pj_ticket').css({transform: `translateX(0px) rotate(-70deg)`});
+            $('.pj_ticket').css({transform: `rotate(-70deg) translateY(200px)`});
         });
 
-        $('.pj_ticket').css({transform: 'rotate(-70deg)'});
+        $('.pj_ticket').css({transform: 'rotate(-70deg) translateY(200px)'});
 
-        // $('.top_ticket').css({position: 'relative'});
-        $('.bottom_ticket').css({position: 'relative',transform: `rotate(0deg) scale(1)`, marginLeft:`0px`});
+        $('.top_ticket').css({position: 'relative', top:0, transform: `rotate(0deg) scale(1)`, marginLeft:`0px`});
+        $('.bottom_ticket').css({position: 'relative', top:0,transform: `rotate(0deg) scale(1)`, marginLeft:`0px`});
     }
     else if(150 < currentScrollTop && currentScrollTop < 500){
         $('.leaflet_wrap').hover(function(){
@@ -38,23 +40,38 @@ $(window).scroll(function () {
             $('.pj_ticket').css({transform: `translateX(0px)`});
         });
         
-        $('.pj_ticket').css({transform: 'rotate(0)'});
+        $('.pj_ticket').css({transform: 'rotate(0)', animation: 'none'});
 
-        // $('.top_ticket').css({position: 'fixed',top:`${currentScrollTop}px`});
+        $('.top_ticket').css({position: 'fixed', top:`${currentScrollTop + 150}px`, 
+            transform: `rotate(-180deg) scale(${ticketScale + (currentScrollTop*0.001)})`,
+            marginLeft:`${currentScrollTop - 100}px`
+        });
 
-        $('.bottom_ticket').css({position: 'fixed',top:`${currentScrollTop + 150}px`,transform: `rotate(70deg) scale(${ticketScale + (currentScrollTop*0.0015)})`,
-        marginLeft:`${currentScrollTop - 200}px`});
+        $('.bottom_ticket').css({position: 'fixed',top:`${currentScrollTop + 150}px`, 
+            transform: `rotate(50deg) scale(${ticketScale + (currentScrollTop*0.0015)})`,
+            marginLeft:`-${currentScrollTop - 150}px`
+        });
 
     }else if(500 < currentScrollTop && currentScrollTop < 800){
-        $('.bottom_ticket').css({position: 'fixed',top:`${currentScrollTop + 150}px`,transform: `rotate(-10deg) scale(${ticketScale + (currentScrollTop*0.002)})`,
-        marginLeft:`-${currentScrollTop - 510}px`});
+        $('.top_ticket').css({position: 'fixed', top:`${currentScrollTop + 150}px`, 
+            transform: `rotate(-220deg) scale(${ticketScale + (currentScrollTop*0.001)})`,
+            marginLeft:`${currentScrollTop - 100}px`
+        });
+
+        $('.bottom_ticket').css({position: 'fixed',top:`${currentScrollTop + 150}px`,
+            transform: `rotate(-10deg) scale(${ticketScale + (currentScrollTop*0.002)})`,
+            marginLeft:`-${currentScrollTop - 150}px`
+        });
 
     }else if(800 < currentScrollTop && currentScrollTop < 1000){
-        $('.bottom_ticket').css({position: 'fixed',top:`${currentScrollTop + 200}px`,transform: `rotate(0deg) scale(${ticketScale + (currentScrollTop*0.002)})`,
-        marginLeft:`${currentScrollTop - 750}px`});
+        $('.top_ticket').css({position: 'fixed', top:`${currentScrollTop + 250}px`, 
+            transform: `rotate(-300deg) scale(${ticketScale + (currentScrollTop*0.001)})`,
+            marginLeft:`-${currentScrollTop - 800}px`
+        });
 
-    }else if(1000 < currentScrollTop){
-        $('.bottom_ticket').css({position: 'fixed',top:`1200px`, transform: `rotate(0deg) scale(3)`, marginLeft:`250px`});
+        $('.bottom_ticket').css({position: 'fixed',top:`${currentScrollTop + 250}px`,transform: `rotate(0deg) scale(${ticketScale + (currentScrollTop*0.002)})`,
+        marginLeft:`${currentScrollTop - 800}px`});
+
     }
 
 
